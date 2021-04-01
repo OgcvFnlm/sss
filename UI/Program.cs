68b4a70ed04263c215f945a82a95398c45e97da0
OgcvFnlm <jmn8126855@163.com>
2021-04-01 09:08:11 +0800

    1121

--- UI/Program.cs
+++ UI/Program.cs
@@ -16,7 +16,7 @@ namespace UI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1("root"));
+            Application.Run(new Form1());
         }
     }
 }
